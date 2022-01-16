0841c78882dbea4e7d0f45188a6044b980ffe72e
Adam <55162814+PanoptesDreams@users.noreply.github.com>
2022-01-17 02:12:51 +1300

    Added program source

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+        }
+    }
+}
